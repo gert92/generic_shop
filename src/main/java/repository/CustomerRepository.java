73fb631085535b2344e43b8873224c2bf730189b
DenisYolo <denis.leping@yolo.com>
2022-09-11 10:36:08 +0300

    CustomerRepository added.

---
+++ src/main/java/repository/CustomerRepository.java
@@ -0,0 +1,84 @@
+package repository;
+
+import dto.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRepository {
+
+    private static final SessionFactory factory = SessionManager.getSessionFactory();
+
+
+    public String createCustomer(Customer customer) {
+        Transaction transaction = null;
+
+        try(Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.out.println(e.getClass() + ": " + e.getMessage());
+        }
+
+        return "Customer created.";
+    }
+
+    public List<Customer> showAllCustomer(){
+        Transaction transaction = null;
+        List<Customer> customers = new ArrayList<>();
+
+        try(Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            customers = session.createQuery("FROM Customer", Customer.class).getResultList();
+            transaction.commit();
+        } catch (Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return customers;
+    }
+
+    public String removeCustomer(Customer customer){
+        Transaction transaction = null;
+        try(Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.remove(customer);
+            transaction.commit();
+        } catch (Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        return "Customer removed.";
+    }
+
+    public String updateCustomer(Customer customer){
+        Transaction transaction = null;
+        try(Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.merge(customer);
+            transaction.commit();
+        } catch (Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        return "Customer updated successfully.";
+    }
+
+
+
+}
